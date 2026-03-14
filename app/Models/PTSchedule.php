bb53eb1c7af6f08afd4249ac42a390a3a67e5974
Lloyd Justin <felecildaj@gmail.com>
2026-03-14 21:28:45 +0800

    Added soft delete

--- app/Models/PTSchedule.php
+++ app/Models/PTSchedule.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Carbon\Carbon;
 
 class PTSchedule extends Model
 {
+    use SoftDeletes;
     protected $table = 'pt_schedules';
 
     protected $fillable = [
